b1f3cc52aa9529f9b9fadda5b03622528b620983
Nail <nadiroglunail@gmail.com>
2024-04-19 16:22:26 -0400

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 const nextConfig = {
     output: 'export',
     images: {
+      loader: 'custom',
+      loaderFile: './loader.js',
       remotePatterns: [
         {
-          loader: 'custom',
-          loaderFile: './loader.js',
           protocol: "https",
           hostname: "images.pexels.com",
         },
